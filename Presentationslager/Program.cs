02ccc570f0124d6d97c9926c545c3aabd0fbf42d
Miriam <88656262+Mininen@users.noreply.github.com>
2021-10-29 15:56:34 +0200

    Add files via upload

---
+++ Presentationslager/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using affärslager;
+
+namespace Presentationslager
+{
+    static class Program
+    {
+        [STAThread]
+        static void Main()
+        {
+            BusinessManager bm = new BusinessManager();
+
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            Application.Run(new Inloggning(bm));
+        }
+    }
+}
